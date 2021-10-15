5f688ff803747812ae2fd75b72e0e21e52cd546a
Arianna Cabebe <aricabebe@gmail.com>
2021-10-14 22:56:45 -0700

    switched blueprint to 'books_bp' in init, created 2 endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-hello_world_bp = Blueprint("hello_world", __name__)
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
 
-# Flask Hello lesson - Endpoint 1 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def hello_world():
-    response = "hello world!"
-    return response
+book1 = Book(1, "On Earth We're Briefly Gorgeous", "A really moving nonfiction")
+book2 = Book(2, "Name of the WOUND", "Cool fantasy, Rothfuss rules")
+book3 = Book(3, "Dune", "Not sure but everyone tells me it's great")
+books = [book1, book2, book3]
 
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    return {
-        "name": "Duke Cabebe",
-        "message": "Hello!",
-        "interests": ["barking", "chasing squirrels", "interrupting zoom calls", "being adorable"]
-    }
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
