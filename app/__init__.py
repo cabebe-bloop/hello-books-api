d41792a9122080f636a038ed410f3940f39a547e
Arianna Cabebe <aricabebe@gmail.com>
2021-10-21 21:16:29 -0700

    added book model, but migration unsuccessful

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from.routes import books_bp
-    app.register_blueprint(books_bp)
+    app.config['SQL_TRACK_MODIFICATIONS'] = False
+    app.config['SQL_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.models.book import Book
+
+    # from.routes import books_bp
+    # app.register_blueprint(books_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,36 @@
-from flask import Blueprint, jsonify
+# from flask import Blueprint, jsonify
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-book1 = Book(1, "On Earth We're Briefly Gorgeous", "A really moving nonfiction")
-book2 = Book(2, "Name of the WOUND", "Cool fantasy, Rothfuss rules")
-book3 = Book(3, "Dune", "Not sure but everyone tells me it's great")
-books = [book1, book2, book3]
+# book1 = Book(1, "On Earth We're Briefly Gorgeous", "A really moving nonfiction")
+# book2 = Book(2, "Name of the WOUND", "Cool fantasy, Rothfuss rules")
+# book3 = Book(3, "Dune", "Not sure but everyone tells me it's great")
+# books = [book1, book2, book3]
 
-books_bp = Blueprint("books", __name__, url_prefix="/books")
+# books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
+#     return jsonify(books_response)
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-            }
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def handle_book(book_id):
+#     book_id = int(book_id)
+#     for book in books:
+#         if book.id == book_id:
+#             return {
+#                 "id": book.id,
+#                 "title": book.title,
+#                 "description": book.description
+#             }
